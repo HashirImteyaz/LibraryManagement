af7f5ce03aa13c36c0cd4c431d56e801eb8978a1
HashirImteyaz <hashirtalks0@gmail.com>
2024-12-21 20:33:36 +0530

    minor updates

--- src/test/java/com/becoder/ApplicationTests.java
+++ src/test/java/com/becoder/ApplicationTests.java
@@ -3,7 +3,9 @@ package com.becoder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.arvind.Application;
+
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
 	@Test
