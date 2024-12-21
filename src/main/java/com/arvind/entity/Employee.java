23a60032e7a85c683bd3f30eee7cc0af2af736b0
HashirImteyaz <hashirtalks0@gmail.com>
2024-12-21 20:07:25 +0530

    ok

---
+++ src/main/java/com/arvind/entity/Employee.java
@@ -0,0 +1,87 @@
+package com.arvind.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String email;
+
+	private String address;
+
+	private String available;
+
+	private String quantity;
+
+	// @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	// @JoinColumn(name = "user_id")
+	// private User user;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(String available) {
+		this.available = available;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + ", available="
+				+ available + ", quantity=" + quantity + "]";
+	}
+
+}
